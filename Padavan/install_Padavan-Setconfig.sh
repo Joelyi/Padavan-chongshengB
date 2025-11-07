9be2f8ab5223f6d51e2235feab51ce986db30573
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-11-08 00:32:43 +0800

    Create install_Padavan-Setconfig.sh

---
+++ Padavan/install_Padavan-Setconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DESTDIR=/opt/rt-n56u
+ROOTDIR=`pwd`
+
+
+    echo "--------------开始复制配置文件----------------------"
+sudo rm -rf ${DESTDIR}/trunk/user/smartdns
+cp -rf ${ROOTDIR}/smartdns ${DESTDIR}/trunk/user
+	      echo "--------------复制配置文件结束------------------"
