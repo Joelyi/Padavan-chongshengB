82217e3650a5debdff4b8324c4dd3b81ba2d618c
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-11-08 00:52:21 +0800

    Update install_Padavan-Setconfig.sh

--- Padavan/install_Padavan-Setconfig.sh
+++ Padavan/install_Padavan-Setconfig.sh
@@ -7,4 +7,7 @@ ROOTDIR=`pwd`
     echo "--------------开始复制配置文件----------------------"
 sudo rm -rf ${DESTDIR}/trunk/user/smartdns
 cp -rf ${ROOTDIR}/smartdns ${DESTDIR}/trunk/user
+cd ${DESTDIR}/trunk
+pwd
+sudo ls -l
 	      echo "--------------复制配置文件结束------------------"
