59c95ae8c44e9984825a62918559c4fedba18012
Joelyi <37099326+Joelyi@users.noreply.github.com>
2025-11-08 17:51:57 +0800

    Update install_Padavan-Setconfig.sh

--- Padavan/install_Padavan-Setconfig.sh
+++ Padavan/install_Padavan-Setconfig.sh
@@ -6,6 +6,7 @@ ROOTDIR=`pwd`
 
 echo "--------------开始复制配置文件----------------------"
 cp -f ${ROOTDIR}/linux-3.4.x/Makefile ${DESTDIR}/trunk/linux-3.4.x/Makefile
+cp -f ${ROOTDIR}/tools/Makefile ${DESTDIR}/trunk/tools/Makefile
 sudo rm -rf ${DESTDIR}/trunk/user/smartdns
 cp -rf ${ROOTDIR}/smartdns ${DESTDIR}/trunk/user
 cd ${DESTDIR}
